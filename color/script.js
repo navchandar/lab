77be96719ce9da79f1561e42ee7cfb4683ed6918
NaveenChandar <12165092+navchandar@users.noreply.github.com>
2025-07-08 00:26:22 +0530

    Adding theme colors

--- color/script.js
+++ color/script.js
@@ -2,15 +2,28 @@ let currentIndex = 0;
 let currentLang = 'english';
 
 
-function getBrightness(hexColor) {
+function getBrightness(color) {
     const ctx = document.createElement('canvas').getContext('2d');
-    ctx.fillStyle = hexColor;
-    const rgb = ctx.fillStyle.match(/\\d+/g).map(Number);
-    return (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
+    ctx.fillStyle = color;
+    const rgbString = ctx.fillStyle;
+    const rgb = rgbString.match(/\d+/g);
+
+    // Assume bright background as fallback
+    if (!rgb || rgb.length < 3) {
+        console.warn(`Invalid color: ${color}`);
+        return 255;
+    }
+
+    // Formula to calculate a weighted average of the three primary colors
+    // Baed on ITU-R BT.601 standard
+    const [r, g, b] = rgb.map(Number);
+    return (r * 299 + g * 587 + b * 114) / 1000;
 }
 
 
-function getTextStyleForBrightness(brightness) {
+
+function getTextStyleForBrightness(color) {
+    const brightness = getBrightness(color);
     const isDark = brightness < 128;
     return {
         textColor: isDark ? 'white' : 'black',
@@ -21,20 +34,35 @@ function getTextStyleForBrightness(brightness) {
     };
 }
 
+function changeBodyColor(color) {
+    // update color with importance to work on devices with Dark viewer
+    document.body.style.setProperty('background-color', color, 'important');
+    // Update theme color on supported mobile devices
+    const metaTag = document.getElementById('theme-color');
+    if (metaTag) {
+        metaTag.setAttribute('content', color);
+    }
+    console.log("Updated color to: " + color);
+}
 
-function updateColor() {
-    const colorData = window.colors[currentLang].names[currentIndex];
-    const color = colorData.color;
-    const label = colorData.label;
-
-    document.body.style.backgroundColor = color;
-    const brightness = getBrightness(color);
-    const { textColor, textShadow } = getTextStyleForBrightness(brightness);
+function changeTextColor(color, label) {
+    const { textColor, textShadow } = getTextStyleForBrightness(color);
 
     const colorNameEl = document.getElementById('color-name');
     colorNameEl.style.color = textColor;
     colorNameEl.style.textShadow = textShadow;
     colorNameEl.textContent = label;
+    console.log("Updated text to: " + label);
+}
+
+function updateColor() {
+    // Get color data and label values
+    const colorData = window.colors[currentLang].names[currentIndex];
+    const color = colorData.color;
+    const label = colorData.label;
+
+    changeBodyColor(color);
+    changeTextColor(color, label);
 
     currentIndex = (currentIndex + 1) % window.colors[currentLang].names.length;
 }
