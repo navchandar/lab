d6b20ca80f7fd07badddcf207b6d54b5f8e7edd4
navchandar <12165092+navchandar@users.noreply.github.com>
2025-11-19 17:30:05 +0000

    build: Auto-generate PWA files

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-// version=v2025.11.19_15.41
+// version=v2025.11.19_22.59
 
 const IGNORED_DIRS = [".git",".github","config","node_modules","Lychee","stefanzweifel","ip","jobs","smart-dom-inspector"];
 
